c94c658ceb366836e9230f5e07287f81676c2b9e
Harry Youd <harry@youd.family>
2025-02-02 13:57:11 +0000

    Attempt to preserve data in event of error

--- cbits_vacation.php
+++ cbits_vacation.php
@@ -6,6 +6,7 @@ class cbits_vacation extends rcube_plugin {
     public $noframe = true;
     public $noajax = true;
     private $rc;
+    public $data;
 
     function init() {
         $this->rc = rcmail::get_instance();
@@ -34,51 +35,65 @@ class cbits_vacation extends rcube_plugin {
     }
 
     function vacation_save() {
-        $this->register_handler('plugin.body', array($this, 'vacation_form'));
+        $this->register_handler('plugin.body', array($this, 'create_form'));
         $this->rc->output->set_pagetitle($this->gettext('setvacation'));
 
-        $data = [
+        // data we will pass to the driver
+        $data_for_backend = [];
+
+        // data we have been sent, and will send back to the client on error
+        $this->data = [
+            'active' => rcube_utils::get_input_value('active', rcube_utils::INPUT_POST),
+            'forwarding_address' => rcube_utils::get_input_value('forwarding_address', rcube_utils::INPUT_POST),
+            'start_datetime' => rcube_utils::get_input_value('start_datetime', rcube_utils::INPUT_POST),
+            'end_datetime' => rcube_utils::get_input_value('end_datetime', rcube_utils::INPUT_POST),
             'message' => rcube_utils::get_input_value('message', rcube_utils::INPUT_POST, true),
-            'forward' => rcube_utils::get_input_value('forwarding_address', rcube_utils::INPUT_POST),
         ];
 
-        if (trim($data['forward']) == '') {
-            $data['forward'] = null;
-        }
+        if ($this->data['active'] == "off") {
+            $data_for_backend['enabled'] = false;
+            $data_for_backend['start_datetime'] = null;
+            $data_for_backend['end_datetime'] = null;
+        } elseif ($this->data['active'] == "on-indef") {
+            $data_for_backend['enabled'] = true;
+            $data_for_backend['start_datetime'] = null;
+            $data_for_backend['end_datetime'] = null;
+        } elseif ($this->data['active'] == "on-dates") {
+            $data_for_backend['enabled'] = true;
 
-        $active = rcube_utils::get_input_value('active', rcube_utils::INPUT_POST);
-
-        if ($active == "off") {
-            $data['enabled'] = false;
-            $data['start_datetime'] = null;
-            $data['end_datetime'] = null;
-        } elseif ($active == "on-indef") {
-            $data['enabled'] = true;
-            $data['start_datetime'] = null;
-            $data['end_datetime'] = null;
-        } elseif ($active == "on-dates") {
-            $data['enabled'] = true;
-            $data['start_datetime'] = DateTimeImmutable::createFromFormat('Y-m-d\TH:i', $data['start_datetime']);
-            if ($data['start_datetime'] === false) {
+            $start_datetime = DateTimeImmutable::createFromFormat('Y-m-d\TH:i', $this->data['start_datetime']);
+            if ($start_datetime === false) {
                 $this->send_error("Start datetime is in an invalid format, needs to be in form of " . date('Y-m-d\TH:i'));
-                return;
             }
-            $data['end_datetime'] = DateTimeImmutable::createFromFormat('Y-m-d\TH:i', $data['end_datetime']);
-            if ($data['end_datetime'] === false) {
+            $data_for_backend['start_datetime'] = $start_datetime;
+
+            $end_datetime = DateTimeImmutable::createFromFormat('Y-m-d\TH:i', $this->data['end_datetime']);
+            if ($end_datetime === false) {
                 $this->send_error("End datetime is in an invalid format, needs to be in form of " . date('Y-m-d\TH:i'));
-                return;
+            }
+            $data_for_backend['end_datetime'] = $end_datetime;
+
+            if ($data_for_backend['start_datetime'] > $data_for_backend['end_datetime']) {
+                $this->send_error("Start datetime should be before end datetime");
             }
         } else {
             $this->send_error('Invalid value for "enabled" field');
-            return;
         }
 
-        if ($this->save($data)) {
+        $data_for_backend['message'] = rcube_utils::get_input_value('message', rcube_utils::INPUT_POST, true);
+        $data_for_backend['forward'] = rcube_utils::get_input_value('forwarding_address', rcube_utils::INPUT_POST);
+
+        if (trim($data_for_backend['forward']) == '') {
+            $data_for_backend['forward'] = null;
+        }
+
+        if ($this->save($data_for_backend)) {
             $this->rc->output->command('display_message', 'Out of Office settings saved successfully', 'confirmation');
         } else {
             $this->rc->output->command('display_message', 'Out of Office settings not saved', 'error');
         }
 
+        $this->data = $this->get();
         $this->rc->overwrite_action('plugin.cbits_vacation');
         $this->rc->output->send('plugin');
     }
@@ -89,38 +104,31 @@ class cbits_vacation extends rcube_plugin {
         $this->rc->output->send('plugin');
     }
 
+    // creates form from data on backend
     function vacation_form() {
-        $data = $this->get();
+        $this->data = $this->get();
+        return $this->create_form();
+    }
 
+    // creates form from data stored in object
+    function create_form() {
+        $data = $this->data;
         $this->rc->html_editor('identity');
-
-        $start_datetime = is_null($data['start_datetime']) ? null : $data['start_datetime']->format('Y-m-d\TH:i');
-        $end_datetime = is_null($data['end_datetime']) ? null : $data['end_datetime']->format('Y-m-d\TH:i');
-
-        if ($data['enabled'] == true) {
-            if ($data['start_datetime'] !== null && $data['end_datetime'] !== null) {
-                $active = "on-dates";
-            } else {
-                $active = "on-indef";
-            }
-        } else {
-            $active = "off";
-        }
 
         $fields = [
             'active' => [
                 html::label('active', rcube::Q($this->gettext('vacenabled'))),
-                html::label([], (new html_radiobutton(['name' => 'active']))->show($active, ['value' => 'off']) . "Off"),
-                html::label([], (new html_radiobutton(['name' => 'active']))->show($active, ['value' => 'on-indef']) . "Enabled indefinitely"),
-                html::label([], (new html_radiobutton(['name' => 'active']))->show($active, ['value' => 'on-dates']) . "Enabled between date range"),
+                html::label([], (new html_radiobutton(['name' => 'active']))->show($data['active'], ['value' => 'off']) . "Off"),
+                html::label([], (new html_radiobutton(['name' => 'active']))->show($data['active'], ['value' => 'on-indef']) . "Enabled indefinitely"),
+                html::label([], (new html_radiobutton(['name' => 'active']))->show($data['active'], ['value' => 'on-dates']) . "Enabled between date range"),
             ],
             'start_datetime' => [
                 html::label('start_datetime', rcube::Q($this->gettext('vacstartdate'))),
-                (new html_inputfield(['name' => 'start_datetime', 'type' => 'datetime-local']))->show($start_datetime),
+                (new html_inputfield(['name' => 'start_datetime', 'type' => 'datetime-local']))->show($data['start_datetime']),
             ],
             'end_datetime' => [
                 html::label('end_datetime', rcube::Q($this->gettext('vacenddate'))),
-                (new html_inputfield(['name' => 'end_datetime', 'type' => 'datetime-local']))->show($end_datetime),
+                (new html_inputfield(['name' => 'end_datetime', 'type' => 'datetime-local']))->show($data['end_datetime']),
             ],
             'forwarding_address' => [
                 html::label('forwarding_address', rcube::Q($this->gettext('vacforward'))),
@@ -158,7 +166,25 @@ class cbits_vacation extends rcube_plugin {
     }
 
     function get() {
-        return $this->driver->get();
+        $data = $this->driver->get();
+
+        if ($data['enabled'] === true) {
+            if ($data['start_datetime'] !== null && $data['end_datetime'] !== null) {
+                $data['active'] = "on-dates";
+            } else {
+                $data['active'] = "on-indef";
+            }
+        } else {
+            $data['active'] = "off";
+        }
+
+        // we use 'active'
+        unset($this->data['enabled']);
+
+        $data['start_datetime'] = is_null($data['start_datetime']) ? null : $data['start_datetime']->format('Y-m-d\TH:i');
+        $data['end_datetime'] = is_null($data['end_datetime']) ? null : $data['end_datetime']->format('Y-m-d\TH:i');
+
+        return $data;
     }
 
     function save($data) {
