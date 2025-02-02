135f7afe1283ac8c83474ffe0efa6ee67073c176
Harry Youd <harry@youd.family>
2025-02-02 13:42:57 +0000

    Update hidden fields on page load

--- cbits_vacation.js
+++ cbits_vacation.js
@@ -5,30 +5,36 @@ window.rcmail && rcmail.addEventListener('init', function(evt) {
 
     new rcube_text_editor({}, 'vacation-message');
 
-    $('input[type=radio][name=active]').change(function() {
-        switch(this.value) {
-            case "off":
-                $('#vacation-start_datetime-row').hide();
-                $('#vacation-end_datetime-row').hide();
-                $('#vacation-forwarding_address-row').hide();
-                $('#vacation-message-row').hide();
-                break;
-            case "on-dates":
-                $('#vacation-start_datetime-row').show();
-                $('#vacation-end_datetime-row').show();
-                $('#vacation-forwarding_address-row').show();
-                $('#vacation-message-row').show();
-                break;
-            case "on-indef":
-                $('#vacation-start_datetime-row').hide();
-                $('#vacation-end_datetime-row').hide();
-                $('#vacation-forwarding_address-row').show();
-                $('#vacation-message-row').show();
-                break;
-            default:
-        };
+    $('input[type=radio][name=active]').change(function () {
+        update_hidden_fields(this.value)
     })
 
+    update_hidden_fields($('input[type=radio][name=active]:checked').val())
 
     $('input:not(:hidden)').first().focus();
 });
+
+
+function update_hidden_fields(value) {
+    switch(value) {
+        case "off":
+            $('#vacation-start_datetime-row').hide();
+            $('#vacation-end_datetime-row').hide();
+            $('#vacation-forwarding_address-row').hide();
+            $('#vacation-message-row').hide();
+            break;
+        case "on-dates":
+            $('#vacation-start_datetime-row').show();
+            $('#vacation-end_datetime-row').show();
+            $('#vacation-forwarding_address-row').show();
+            $('#vacation-message-row').show();
+            break;
+        case "on-indef":
+            $('#vacation-start_datetime-row').hide();
+            $('#vacation-end_datetime-row').hide();
+            $('#vacation-forwarding_address-row').show();
+            $('#vacation-message-row').show();
+            break;
+        default:
+    }
+}
